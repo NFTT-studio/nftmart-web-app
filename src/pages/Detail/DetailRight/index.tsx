5249adcadfe65787fc679251acd1ed0e8f11bf3d
lrqgithub <1819899940@qq.com>
2021-10-30 11:40:13 +0800

    chore: fix event

--- src/pages/Detail/DetailRight/index.tsx
+++ src/pages/Detail/DetailRight/index.tsx
@@ -1170,9 +1170,6 @@ const DetailRight: FC<Props> = (({
                   {eventDate?.pages.map((page) => page?.events?.map((item) => (
                     item.method === 'BurnnedToken'
                     || item.method === 'BurnRemovedBritishAuctionnedToken'
-                    || item.method === 'RemovedDutchAuction'
-                    || item.method === 'RemovedOrder'
-                    || item.method === 'RemovedOffer'
                       ? null : (<Activity events={item} />)
                   )))}
                 </InfiniteScroll>
