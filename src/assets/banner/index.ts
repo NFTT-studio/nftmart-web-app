7f1f7cb54a0200f35f68c9e1bb011527589d1538
lrqgithub <1819899940@qq.com>
2022-01-10 20:48:38 +0800

    chore: fix update banner

--- src/assets/banner/index.ts
+++ src/assets/banner/index.ts
@@ -4,6 +4,8 @@ export * as banner03 from './003.png';
 export * as banner04 from './004.png';
 export * as banner05 from './005.png';
 export * as banner06 from './006.png';
+export * as banner07 from './007.png';
+export * as banner08 from './008.png';
 export * as banner09 from './009.png';
 export * as banner10 from './010.png';
 export * as bannerLeft from './banner_left.png';
